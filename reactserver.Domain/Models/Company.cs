59ce89ef731c397873d82b67c79b19f110dacdd7
Евгений Елизаров <e.elizarov@appricot.ru>
2023-08-10 12:49:29 +0300

    save changes

--- reactserver.Domain/Models/Company.cs
+++ reactserver.Domain/Models/Company.cs
@@ -1,4 +1,6 @@
-﻿namespace reactserver.Domain.Models
+﻿using Newtonsoft.Json;
+
+namespace reactserver.Domain.Models
 {
     public class Company
     {
@@ -7,6 +9,25 @@
         public string INN { get; set; }
         public string OGRN { get; set; }
         public DateTime DateRegistration { get; set; }
+        public void SetDateRegistration(string dateRegistrationString)
+        {
+            if (DateTime.TryParse(dateRegistrationString, out DateTime dateRegistration))
+            {
+                DateRegistration = dateRegistration.ToUniversalTime();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid date format.");
+            }
+        }
 
+    }
+   public class CompanyRequest
+    {
+        public string FullName { get; set; }
+        public string INN { get; set; }
+        public string OGRN { get; set; }
+        [JsonProperty("dateRegistration")]
+        public string DateRegistration { get; set; }
     }
 }
--- reactserver/Controllers/CompanyController.cs
+++ reactserver/Controllers/CompanyController.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using reactserver.database;
 using reactserver.Domain.Models;
 using System.ComponentModel.Design;
 
 namespace reactserver.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class CompanyController : ControllerBase
     {
+        private AppDbContext db;
+
+        public CompanyController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
         [HttpGet]
         [Route("GetCompany")]
         public Company GetCompanyes()
         {
             var company = new Company();
 
-            company.DateRegistration = DateTime.Now;
             company.FullName = "Что попало";
             company.Id = Guid.NewGuid();
             company.INN = "d222233111";
@@ -22,6 +30,29 @@ namespace reactserver.Controllers
 
            
             return company;
+        }
+        [HttpPost]
+        [Route("CreateCompany")]
+        public IActionResult CreateCompany([FromBody] CompanyRequest request)
+        {
+            var company = new Company();
+
+            company.SetDateRegistration(request.DateRegistration);
+            company.FullName = request.FullName;
+            company.Id = Guid.NewGuid();
+            company.INN = request.INN;
+            company.OGRN = request.OGRN;
+            db.Companies.Add(company);
+            int affectedRows = db.SaveChanges();
+
+            if (affectedRows > 0)
+            {
+                return Ok(new { message = "Компания успешно создана", company });
+            }
+            else
+            {
+                return BadRequest(new { message = "Ошибка создания компании" });
+            }
         }
     }
 }
